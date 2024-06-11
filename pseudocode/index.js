fee62894b8abe597c61d32f4c3295ec52e7c7964
jaykrator <umerahchikaodi@gmail.com>
2024-06-11 21:22:26 +0100

    first commit

---
+++ pseudocode/index.js
@@ -0,0 +1,15 @@
+//year of birth
+let yearofbirth = 1995;
+//current year
+let currentyear = 2024;
+//calculate the different between current year and year of birth
+let difference = currentyear - yearofbirth;
+
+//remainder value between two numbers
+let firstnumber = 9;
+//second number
+let secondnumber = 5;
+//remainder value
+let remaindervalue = firstnumber % secondnumber;
+//return remainder
+console.log(remaindervalue);
